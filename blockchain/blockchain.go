cfc781481b89142301a2d4f1787b10a684569c3c
orackle <ranjana1997@gmail.com>
2025-06-21 01:15:00 -0400

    blockchain file error

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,15 +1,29 @@
 package blockchain
-type BlockChain struct{
-	Blocks	[]*Block
+
+type BlockChain struct {
+	Blocks []*Block
 }
 
+type Transaction struct {
+	Sender   string
+	Receiver string
+	Amount   float64
+	Coinbase bool
+}
 
 func InitBlockChain() *BlockChain {
-  return &BlockChain{[]*Block{Genesis()}}
+	return &BlockChain{[]*Block{Genesis()}}
 }
 
-func (chain *BlockChain) AddBlock(data string){
-  prevBlock := chain.Blocks[len(chain.Blocks)-1]
-  newBlock := CreateBlock(data, prevBlock.Hash)
-  chain.Blocks = append(chain.Blocks, newBlock)
+func (chain *BlockChain) AddBlock(data string, coinbaseRcpt string, transactions []*Transaction) {
+
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	coinbaseTransaction := &Transaction{
+		Sender: "Coinbase",
+		Receiver: coinbaseRcpt,
+		Amount: 10.0,
+		Coinbase: true,
+	}
+	newBlock := CreateBlock(data, prevBlock.Hash, append([]*Transaction{coinbaseTransaction}, transactions...))
+	chain.Blocks = append(chain.Blocks, newBlock)
 }
